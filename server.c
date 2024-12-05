64ad6b06d1da59e4509612d42ef72d7434723587
highSpeedcoolGuy <143373453+highSpeedcoolGuy@users.noreply.github.com>
2024-12-04 20:24:41 -0700

    Update server.c

--- server.c
+++ server.c
@@ -31,27 +31,22 @@ int main() {
 	while (1) {
 		// TODO:
 		// read requests from serverFIFO
-
-
-
-
-
-
+		if (read(server,&req,sizeof(struct message))!=sizeof(struct message)) {
+			continue;
+		}
 		printf("Received a request from %s to send the message %s to %s.\n",req.source,req.msg,req.target);
-
+		struct message response;
+		target = open(req.target, O_WRONLY);
+		strcpy(response.msg,req.msg);
+		strcpy(response.source,req.source);
+		strcpy(response.target,req.target);
+		write(target,&response,sizeof(struct message));
+		close(target);
 		// TODO:
 		// open target FIFO and write the whole message struct to the target FIFO
 		// close target FIFO after writing the message
-
-
-
-
-
-
-
 	}
 	close(server);
 	close(dummyfd);
 	return 0;
 }
-
